dc60ac920f8cdde579278b3ea13450e9d47ee775
XenoAmess <912890489@qq.com>
2018-04-11 01:44:08 +0800

    use hand-made list insteadof the original list.

--- src/HashTable.java
+++ src/HashTable.java
@@ -1,104 +1,132 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+public class HashTable<K, V> {
+	static final int HASH_BITS = 0x7fffffff; // usable bits of normal node hash
 
-public class HashTable<T> {
+	/**
+	 * Spreads (XORs) higher bits of hash to lower and also forces top bit to 0.
+	 * Because the table uses power-of-two masking, sets of hashes that vary only in
+	 * bits above the current mask will always collide. (Among known examples are
+	 * sets of Float keys holding consecutive whole numbers in small tables.) So we
+	 * apply a transform that spreads the impact of higher bits downward. There is a
+	 * tradeoff between speed, utility, and quality of bit-spreading. Because many
+	 * common sets of hashes are already reasonably distributed (so don't benefit
+	 * from spreading), and because we use trees to handle large sets of collisions
+	 * in bins, we just XOR some shifted bits in the cheapest possible way to reduce
+	 * systematic lossage, as well as to incorporate impact of the highest bits that
+	 * would otherwise never be used in index calculations because of table bounds.
+	 */
+	static final int spread(int h) {
+		return (h ^ (h >>> 16)) & HASH_BITS;
+	}
 
-	class pair {
-		protected T key;
-		protected int val;
+	class Pair {
+		protected final K key;
+		protected volatile V value;
 
-		// pair(){
-		// val = 0;
-		// }
-		pair(T key) {
+		Pair(K key, V value) {
 			this.key = key;
-			val = 1;
+			this.value = value;
 		}
 	}
 
-	public ArrayList<LinkedList<pair>> pool;
+	class Node {
+		protected final Node nextNode;
+		protected final Pair pair;
+
+		Node(Node nextNode, Pair pair) {
+			this.nextNode = nextNode;
+			this.pair = pair;
+		}
+	}
+
+	// class Table {
+	//
+	// }
+
+	public Node[] pool;
+
 	int nowPoolSize;
 	int nodeSize;
 
 	public HashTable() {
 		super();
-		init(50);
+		init(128);
 	}
 
-	public HashTable(int initialPoolSize) {
+	public HashTable(int nowPoolSize) {
 		super();
-		init(initialPoolSize);
+		init(nowPoolSize);
 	}
 
-	private void init(int initialPoolSize) {
-		this.nowPoolSize = initialPoolSize * 2 + 1;
-		this.pool = new ArrayList(this.nowPoolSize);
+	@SuppressWarnings("unchecked")
+	private void init(int nowPoolSize) {
+		this.nowPoolSize = nowPoolSize;
+		this.pool = (HashTable<K, V>.Node[]) new Object[this.nowPoolSize];
 		this.nodeSize = 0;
 	}
 
-	public int get(T t) {
-		int hashCode = t.hashCode();
+	public V get(K k) {
+		int hashCode = spread(k.hashCode());
 		int nowHashCode = hashCode % nowPoolSize;
-		List<pair> list = pool.get(nowHashCode);
-		if (list == null) {
-			return 0;
-		}
-		for (pair au : list) {
-			T nowt = au.key;
-			if (nowt.equals(t)) {
-				return au.val;
-			}
+		Node nowNode = pool[nowHashCode];
+		if (nowNode == null) {
+			return null;
 		}
 
-		return 0;
+		while (nowNode != null) {
+			if (nowNode.pair.key.equals(k)) {
+				return nowNode.pair.value;
+			}
+			nowNode = nowNode.nextNode;
+		}
+
+		return null;
 	}
 
-	public void insert(T t) {
-		int hashCode = t.hashCode();
+	public void insert(K k, V v) {
+		int hashCode = spread(k.hashCode());
 		int nowHashCode = hashCode % nowPoolSize;
-		List<pair> list = pool.get(nowHashCode);
-		if (list == null) {
-			list = new LinkedList<pair>();
-			pool.set(nowHashCode, (LinkedList<pair>) list);
-		}
-		for (pair au : list) {
-			T nowt = au.key;
-			if (nowt.equals(t)) {
-				au.val++;
-			}
+
+		Node nowNode = pool[nowHashCode];
+		if (nowNode == null) {
+			nowNode = new Node(null, new Pair(k, v));
+			pool[nowHashCode] = nowNode;
 			return;
 		}
 
-		list.add(new pair(t));
+		while (nowNode != null) {
+			if (nowNode.pair.key.equals(k)) {
+				nowNode.pair.value = v;
+				return;
+			}
+			nowNode = nowNode.nextNode;
+		}
+		pool[nowHashCode] = new Node(pool[nowHashCode], new Pair(k, v));
 	}
 
-	public boolean delete(T t) {
-		int hashCode = t.hashCode();
+	public boolean delete(K k) {
+		int hashCode = spread(k.hashCode());
 		int nowHashCode = hashCode % nowPoolSize;
-		List<pair> list = pool.get(nowHashCode);
-		if (list == null) {
+
+		Node nowNode = pool[nowHashCode];
+		Node oldNode = nowNode;
+
+		if (nowNode == null) {
 			return false;
 		}
 
-		LinkedList<pair> list2 = new LinkedList<pair>();
-
-		boolean res = false;
-
-		for (pair au : list) {
-			T nowt = au.key;
-			if (nowt.equals(t)) {
-				// list.erase(au);
-				res = true;
-			} else {
-				list2.add(au);
+		while (nowNode != null) {
+			if (nowNode.pair.key.equals(k)) {
+				Node newNode = nowNode;
+				while (oldNode != nowNode) {
+					newNode = new Node(newNode, oldNode.pair);
+					oldNode = oldNode.nextNode;
+				}
+				pool[nowHashCode] = newNode;
+				return true;
 			}
+			nowNode = nowNode.nextNode;
 		}
-		if (res) {
-			list.clear();
-			pool.set(nowHashCode, list2);
-		}
-		return res;
+		return false;
 	}
 
 }
